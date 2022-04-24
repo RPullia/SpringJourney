99cd7739e06cbb0898a4ea07855fae274fe5e535
RPullia <robertopullia@gmail.com>
2022-04-24 11:16:23 +0100

    FIrst commit

---
+++ e2-spring-context/src/main/java/config/ProjectConfig.java
@@ -0,0 +1,12 @@
+package config;
+
+import beans.AnotherBean;
+import beans.MyBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ProjectConfig {
+
+}
