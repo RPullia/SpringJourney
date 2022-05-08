3b9089f7c7d2704f4ff650dff2dea1c1243084c0
RPullia <robertopullia@gmail.com>
2022-05-08 17:26:48 +0100

    E8 completed

--- e8-spring-Transactional/src/main/java/main/Main.java
+++ e8-spring-Transactional/src/main/java/main/Main.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import services.BookService;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
         BookService bs = context.getBean(BookService.class);
